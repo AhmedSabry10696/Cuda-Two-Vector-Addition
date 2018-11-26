e8a603a28df7999f25fbd841182dd758adabd9bc
AhmedSabry10696 <ahmed.sabry1460@el-eng.menofia.edu.eg>
2018-11-26 15:32:28 -0800

    first commit

---
+++ CUDA Addition Of Two Vector.cu
@@ -0,0 +1,72 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <iostream>
+#include <stdio.h>
+#include <assert.h>
+#include <cuda.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <time.h>
+
+// function generate random numbers and assign it to array
+void random_ints(int* a, int N)
+{
+	int i;
+	for (i = 0; i < N; ++i)
+		a[i] = rand();
+}
+
+// create kernal "two vector addition"
+__global__ void add(int *a, int *b, int *c) {
+	int index = threadIdx.x + blockIdx.x * blockDim.x;
+	c[threadIdx.x] = a[threadIdx.x] + b[threadIdx.x];
+	c[index] = a[index] + b[index];
+}
+
+#define N 8                      // array size 
+#define THREADS_PER_BLOCK 2
+
+int main(void) {
+
+	int *a, *b, *c;              // host data 
+	int *d_a, *d_b, *d_c;		 // device data
+	int size = N * sizeof(int);  
+
+	printf("\t\t\t*** CUDA TASK ***\n\t\t\t-----------------\n\n");
+
+	// alloacate host data 
+	a = (int *)malloc(size); random_ints(a, N);
+	b = (int *)malloc(size); random_ints(b, N);
+	c = (int *)malloc(size);
+
+	// allocate device data
+	cudaMalloc((void **)&d_a, size);
+	cudaMalloc((void **)&d_b, size);
+	cudaMalloc((void **)&d_c, size);
+
+	// copy data from host to device
+	cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+
+	// call add kernal 
+	add << < N / THREADS_PER_BLOCK, THREADS_PER_BLOCK >> > (d_a, d_b, d_c);
+
+	// copy data back from device to host
+	cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+
+	printf("A\tB\tC\n------------------------\n");
+
+	for (int i = 0; i < N; i++) {
+		printf("%d\t%d\t%d\n", a[i], b[i], c[i]);
+		printf("------------------------\n");
+	}
+
+	// free allocated host data
+	free(a);  free(b);  free(c);
+
+	// free allocated device data 
+	cudaFree(d_a);  cudaFree(d_b);  cudaFree(d_c);
+
+	return 0;
+}
